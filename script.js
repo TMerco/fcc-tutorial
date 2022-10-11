99adef609c0f09c783e4469576465761a2697a9e
TMerco <tyrellcode@gmail.com>
2022-10-11 10:29:58 -0700

    Day 2 continued

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ undefined, null, boolean, string, symbol, number, and object
 
 
 // Declarations
+
 var studlyCapVar;
 var properCamelCase;
 var titleCaseOver;
@@ -18,4 +19,90 @@ titleCaseOver = 9000;
 //Adding Operator
 var sum = 10 + 0;
 
-console.log(sum);
+//incromenting numbers
+
+var myVar = 87;
+
+myVar = myVar + 1;
+myVar++; /* Cleaner Way */
+
+//decrementing numbers
+
+myVar2 = 11;
+
+myVar2 = myVar2 - 1;
+myVar2--; /* Cleaner Way  */
+
+//Decimal Numbers
+//Also called "FLOATS" or "Floating point numbers"
+
+var ourDecimal = 5.7;
+
+var myDecimal = 0.009;
+
+//Multiplying Decimals
+//Same as Integers
+
+var product = 2.0 * 2.5;
+
+//Finding Remainder
+
+var remainder;
+
+remainder = 11 % 3;
+
+//Compound Assignment with Augmented Addition/Subtraction/Multiplication/Division
+
+var a = 17;
+var b = 3;
+var c = 12;
+var d = 15;
+
+a = a + 12;
+a += 12; /* same thing as above */
+
+b = b - 9;
+b -= 9; /* same thing as above */
+
+c = 7 * c;
+c*= 7; /* same thing as above */
+
+d = d / 5;
+d /= 5; /* same thing as above */
+
+//String Variables
+
+var myFirstName = "Tyrell";
+var myLastName = "Mercolino";
+
+//Quotes inside a Sting
+
+var myStr = "I am a "double quoted" string inside "double quotes"";
+
+//Adding \ makes JS see the line as 1 full quote
+var myStr = "I am a \"double quoted\" string inside \"double quotes\""; 
+
+//Quoting Strings with Single Quotes
+var myStr2 = "<a href=\"https://www.youtube.com\" target=\"_blank\">Link</a>";
+
+//Using single quotes around the outside allows the use of double quotes on the inside of the string without \
+var myStr2 = '<a href="https://www.youtube.com" target="_blank">Link</a>';
+
+//Combining Strings
+
+var ourStr = "I come first. " + "I come second.";
+var myStr3 = "This is the start. " + "This is the end.";
+
+//Integrading Variables with Strings
+
+var myName = "Tyrell";
+//Add Variable Name inbetween Quotes
+var myStr4 = "Hello, my name is " + myName + ", nice to meet you."; /* Output: Hello, my name is Tyrell, nice to meet you.*/
+
+// Append Variables to Strings
+
+
+
+
+//video: https://www.youtube.com/watch?v=PkZNo7MFNFg&t=376s
+// time: 14:50
