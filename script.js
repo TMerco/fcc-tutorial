7e6dc2a380c261474a623f3f4b835d682d2edc6e
Tyrell Mercolino <Tyrell.Mercolino@alaskaair.com>
2022-10-10 17:37:59 -0700

    beginning

---
+++ script.js
@@ -0,0 +1,21 @@
+/* Data Types:
+undefined, null, boolean, string, symbol, number, and object
+*/
+
+
+// Declarations
+var studlyCapVar;
+var properCamelCase;
+var titleCaseOver;
+
+
+//Assignments
+
+studlyCapVar = 10;
+properCamelCase = "A String.";
+titleCaseOver = 9000;
+
+//Adding Operator
+var sum = 10 + 0;
+
+console.log(sum);
